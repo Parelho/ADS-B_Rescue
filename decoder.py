a57cd7661e71340af2ef6f5ae5ed742782def1ef
Parelho <vinicius.parelho@outlook.com>
2026-03-24 13:49:00 -0300

    feat: aircraft class

--- decoder.py
+++ decoder.py
@@ -1,8 +1,17 @@
 import pyModeS as pms
 from pyModeS.extra.tcpclient import TcpClient
 
-adsb_dict = []
+aircrafts = []
 det_coords = [-23.647143, -46.574283]
+
+class Aircraft():
+    icao: int
+    lat: float
+    lon: float
+    speed: int
+    heading: float
+    vertical_rate: int
+    speed_type: str
 
 # define your custom class by extending the TcpClient
 #   - implement your handle_messages() methods
@@ -13,30 +22,53 @@ class ADSBClient(TcpClient):
     def handle_messages(self, messages):
         # print(messages)
         for msg, ts in messages:
-          msg = msg[12:]
-          # print(msg)
-          if len(msg) != 28:  # wrong data length
-              # print("Data length wrong")
-              continue
+            msg = msg[12:]
+            # print(msg)
+            if len(msg) != 28:  # wrong data length
+                # print("Data length wrong")
+                continue
 
-          df = pms.df(msg)
+            df = pms.df(msg)
 
-          if df != 17:  # not ADSB
-              print("Not ADS-B")
-              continue
+            if df != 17:  # not ADSB
+                print("Not ADS-B")
+                continue
 
-          if pms.crc(msg) !=0:  # CRC fail
-              print("CRC failed")
-              continue
+            if pms.crc(msg) != 0:  # CRC fail
+                print("CRC failed")
+                continue
 
-          icao = pms.adsb.icao(msg)
-          tc = pms.adsb.typecode(msg)
+            icao = pms.adsb.icao(msg)
+            if icao not in aircrafts:
+                aircraft: Aircraft = {
+                    "icao": icao,
+                    "lat": None,
+                    "lon": None,
+                    "speed": None,
+                    "heading": None,
+                    "vertical_rate": None,
+                    "speed_type": None
+                }
+                aircrafts.append(aircraft)
 
-          # TODO: write you magic code here
-          # print(ts, icao, tc, msg)
-          if 5 <= tc <= 18:
-            print(pms.adsb.position_with_ref(msg, det_coords[0], det_coords[1]))
+            aircraft = next(item for item in aircrafts if item["icao"] == icao)
+            
+            tc = pms.adsb.typecode(msg)
 
+            if 5 <= tc <= 18:
+                lat, lon = pms.adsb.position_with_ref(msg, det_coords[0], det_coords[1]) # aircraft positon based on base station position
+                # print(f"lat: {lat}, lon: {lon}")
+                aircraft["lat"] = lat
+                aircraft["lon"] = lon
+            elif tc == 19:
+                velocity = pms.adsb.velocity(msg)
+                # print(f"speed: {velocity[0]}, heading: {velocity[1]}, vertical rate: {velocity[2]}, speed type: {velocity[3]}")
+                aircraft["speed"] = velocity[0]
+                aircraft["heading"] = velocity[1]
+                aircraft["vertical_rate"] = velocity[2]
+                aircraft["speed_type"] = velocity[3]
+
+            print(aircrafts)
 
 # run new client, change the host, port, and rawtype if needed
 client = ADSBClient(host='10.33.133.22', port=30002, rawtype='raw')
