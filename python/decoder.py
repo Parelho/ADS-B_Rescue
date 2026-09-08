c8f3df6c221097cf7449413e64b9cd80a21db584
Parelho <vinicius.parelho@outlook.com>
2026-09-08 16:04:47 -0300

    fix: decoder database updates

--- python/decoder.py
+++ python/decoder.py
@@ -1,9 +1,32 @@
 import pyModeS as pms
-from pyModeS.extra.tcpclient import TcpClient
 import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 import time
+
+def fetch_all_rows(table_name):
+    all_data = []
+    page_size = 1000
+    start = 0
+    
+    while True:
+        # Fetch a chunk of 1000 rows
+        response = (
+            supabase.table(table_name)
+            .select("*")
+            .eq("used", "false")
+            .range(start, start + page_size - 1) # Inclusive bounds (e.g., 0 to 999)
+            .execute()
+        )
+        
+        data = response.data
+        if not data:
+            break  # Stop when no more rows are returned
+            
+        all_data.extend(data)
+        start += page_size  # Move to the next page
+        
+    return all_data
 
 # define your custom class by extending the TcpClient
 #   - implement your handle_messages() methods
@@ -75,153 +98,158 @@ class ADSBClient():
 
     def handle_messages(self):
         # print(messages)
-        for msg in messages:
-            if msg["used"] == False:
-                # msg = msg.replace("*", "")
-                # msg = msg.replace(";", "")
-                if len(msg) != 28:  # wrong data length
-                    # print("Data length wrong")
-                    continue
-
-                df = pms.df(msg)
-
-                if df != 17:  # not ADSB
-                    print("Not ADS-B")
-                    continue
-
-                if pms.crc(msg) != 0:  # CRC fail
-                    print("CRC failed")
-                    continue
-                
+        for message in messages:
+            try:
                 # print(msg)
-                # with open("output.txt", "a") as f:
-                #     f.write(str(msg) + "\n")
-                timestamp = int(time.time())
-                # response = (
-                #     supabase.table("raw_msg")
-                #     .insert({
-                #         "msg": msg,
-                #         "timestamp": timestamp
-                #     })
-                #     .execute()
-                # )
-                # print(response)
+                if message["used"] == False:
+                    msg = message["msg"]
+                    # msg = msg.replace("*", "")
+                    # msg = msg.replace(";", "")
+                    if len(msg) != 28:  # wrong data length
+                        # print("Data length wrong")
+                        continue
 
-                icao = pms.adsb.icao(msg)
-                tc = pms.adsb.typecode(msg)
+                    df = pms.df(msg)
 
-                # --- CALLSIGN (Type Codes 1–4) ---
-                if 1 <= tc <= 4:
-                    callsign = pms.adsb.callsign(msg).strip()
-                    self.callsigns[icao] = callsign
-                    print(f"Callsign updated: {icao} -> {callsign}")
+                    if df != 17:  # not ADSB
+                        print("Not ADS-B")
+                        continue
 
-                    self.flush_buffer(icao)
-                    continue
+                    if pms.crc(msg) != 0:  # CRC fail
+                        print("CRC failed")
+                        continue
+                    
+                    # print(msg)
+                    # with open("output.txt", "a") as f:
+                    #     f.write(str(msg) + "\n")
+                    timestamp = int(time.time())
+                    # response = (
+                    #     supabase.table("raw_msg")
+                    #     .insert({
+                    #         "msg": msg,
+                    #         "timestamp": timestamp
+                    #     })
+                    #     .execute()
+                    # )
+                    # print(response)
 
-                callsign = self.callsigns.get(icao)
+                    icao = pms.adsb.icao(msg)
+                    tc = pms.adsb.typecode(msg)
 
-                aircraft = {
-                    "icao": icao,
-                    "lat": None,
-                    "lon": None,
-                    "altitude": None,
-                    "speed": None,
-                    "heading": None,
-                    "vertical_rate": None,
-                    "speed_type": None
-                }
+                    # --- CALLSIGN (Type Codes 1–4) ---
+                    if 1 <= tc <= 4:
+                        callsign = pms.adsb.callsign(msg).strip()
+                        self.callsigns[icao] = callsign
+                        print(f"Callsign updated: {icao} -> {callsign}")
 
-                # --- POSITION ---
-                if 5 <= tc <= 18:
-                    lat, lon = pms.adsb.position_with_ref(msg, det_coords[0], det_coords[1]) # aircraft positon based on base station position
-                    # print(f"lat: {lat}, lon: {lon}")
-                    aircraft["lat"] = lat
-                    aircraft["lon"] = lon
+                        self.flush_buffer(icao)
+                        continue
 
-                    if 9 <= tc <= 18:
-                        aircraft["altitude"] = pms.adsb.altitude(msg)
+                    callsign = self.callsigns.get(icao)
 
-                    if callsign:
-                        response = (
-                            supabase.table("position_data")
-                            .insert({
-                                "icao": icao,
-                                "callsign": callsign,
+                    aircraft = {
+                        "icao": icao,
+                        "lat": None,
+                        "lon": None,
+                        "altitude": None,
+                        "speed": None,
+                        "heading": None,
+                        "vertical_rate": None,
+                        "speed_type": None
+                    }
+
+                    # --- POSITION ---
+                    if 5 <= tc <= 18:
+                        lat, lon = pms.adsb.position_with_ref(msg, det_coords[0], det_coords[1]) # aircraft positon based on base station position
+                        # print(f"lat: {lat}, lon: {lon}")
+                        aircraft["lat"] = lat
+                        aircraft["lon"] = lon
+
+                        if 9 <= tc <= 18:
+                            aircraft["altitude"] = pms.adsb.altitude(msg)
+
+                        if callsign:
+                            response = (
+                                supabase.table("position_data")
+                                .insert({
+                                    "icao": icao,
+                                    "callsign": callsign,
+                                    "lat": lat,
+                                    "lon": lon,
+                                    "altitude": aircraft["altitude"],
+                                    "timestamp": timestamp
+                                })
+                                .execute()
+                            )
+                            print(response)
+
+                            response = (
+                                supabase.table("raw_msg")
+                                .update({"used": True})
+                                .eq("id", message["id"])
+                                .execute()
+                            )
+                            print(response)
+                        else:
+                            # buffer it
+                            self.buffers.setdefault(icao, []).append({
+                                "msg_id": message["id"],
+                                "type": "position",
                                 "lat": lat,
                                 "lon": lon,
                                 "altitude": aircraft["altitude"],
                                 "timestamp": timestamp
                             })
-                            .execute()
-                        )
-                        print(response)
+                            # print("buffered")
 
-                        response = (
-                            supabase.table("raw_msg")
-                            .update({"used": True})
-                            .eq("id", msg["id"])
-                            .execute()
-                        )
-                        print(response)
-                    else:
-                        # buffer it
-                        self.buffers.setdefault(icao, []).append({
-                            "msg_id": msg["id"],
-                            "type": "position",
-                            "lat": lat,
-                            "lon": lon,
-                            "altitude": aircraft["altitude"],
-                            "timestamp": timestamp
-                        })
-                        # print("buffered")
+                    # --- VELOCITY ---
+                    elif tc == 19:
+                        velocity = pms.adsb.velocity(msg)
+                        # print(f"speed: {velocity[0]}, heading: {velocity[1]}, vertical rate: {velocity[2]}, speed type: {velocity[3]}")
+                        aircraft["speed"] = int(velocity[0])
+                        aircraft["heading"] = int(velocity[1])
+                        aircraft["vertical_rate"] = int(velocity[2])
+                        aircraft["speed_type"] = velocity[3]
 
-                # --- VELOCITY ---
-                elif tc == 19:
-                    velocity = pms.adsb.velocity(msg)
-                    # print(f"speed: {velocity[0]}, heading: {velocity[1]}, vertical rate: {velocity[2]}, speed type: {velocity[3]}")
-                    aircraft["speed"] = int(velocity[0])
-                    aircraft["heading"] = int(velocity[1])
-                    aircraft["vertical_rate"] = int(velocity[2])
-                    aircraft["speed_type"] = velocity[3]
+                        if callsign:
+                            response = (
+                                supabase.table("velocity_data")
+                                .insert({
+                                    "icao": icao,
+                                    "callsign": callsign,
+                                    "speed": aircraft["speed"],
+                                    "heading": aircraft["heading"],
+                                    "vertical_rate": aircraft["vertical_rate"],
+                                    "speed_type": aircraft["speed_type"],
+                                    "timestamp": timestamp
+                                })
+                                .execute()
+                            )
+                            print(response)
 
-                    if callsign:
-                        response = (
-                            supabase.table("velocity_data")
-                            .insert({
-                                "icao": icao,
-                                "callsign": callsign,
+                            response = (
+                                supabase.table("raw_msg")
+                                .update({"used": True})
+                                .eq("id", message["id"])
+                                .execute()
+                            )
+                            print(response)
+                        else:
+                            # buffer it
+                            self.buffers.setdefault(icao, []).append({
+                                "msg_id": message["id"],
+                                "type": "velocity",
                                 "speed": aircraft["speed"],
                                 "heading": aircraft["heading"],
                                 "vertical_rate": aircraft["vertical_rate"],
                                 "speed_type": aircraft["speed_type"],
                                 "timestamp": timestamp
                             })
-                            .execute()
-                        )
-                        print(response)
+                            # print("buffered")
 
-                        response = (
-                            supabase.table("raw_msg")
-                            .update({"used": True})
-                            .eq("id", msg["id"])
-                            .execute()
-                        )
-                        print(response)
-                    else:
-                        # buffer it
-                        self.buffers.setdefault(icao, []).append({
-                            "msg_id": msg["id"],
-                            "type": "velocity",
-                            "speed": aircraft["speed"],
-                            "heading": aircraft["heading"],
-                            "vertical_rate": aircraft["vertical_rate"],
-                            "speed_type": aircraft["speed_type"],
-                            "timestamp": timestamp
-                        })
-                        # print("buffered")
-
-                # print(aircrafts)
+                    # print(aircrafts)
+            except Exception as e:
+                print(f"Error: {e}")
 
 
 det_coords = [-21.8877932497327, -49.013558624205324]
@@ -231,16 +259,14 @@ url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-response = (
-    supabase
-    .table("raw_msg")
-    .select("*")
-    .execute()
-)
-
-# Store the result in a dictionary
-messages = response.data
-
 # run new client, change the host, port, and rawtype if needed
 client = ADSBClient()
-client.handle_messages()
+
+while True:
+    # Store the result in a dictionary
+    messages = fetch_all_rows("raw_msg")
+    # print(messages)
+
+    client.handle_messages()
+
+    time.sleep(5)
